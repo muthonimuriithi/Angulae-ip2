3ede3c3102171232c33808d341408f96d6fbaeca
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-18 20:05:31 +0300

    created class repo

---
+++ src/app/repo.ts
@@ -0,0 +1,11 @@
+export class Repo {
+    constructor(public name: string,
+         public full_name: string,
+          public description: string,
+           public updated_at:Date,
+           public html_url: string,
+            public clone_url: string, 
+            public language: string,
+             public created_at:Date){}
+}
+
