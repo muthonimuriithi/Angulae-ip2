33a9d4c2881e748728bd2d959f5db9de5a9e7e6e
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-18 18:49:54 +0300

    imported search results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
+// import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FormComponent,
-    SearchResultsComponent
+    // SearchResultsComponent
   ],
   imports: [
     BrowserModule,
