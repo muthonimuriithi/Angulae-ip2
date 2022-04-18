40152e24038acb198cfe2261f8bb6a46b4f5be6e
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-18 20:07:02 +0300

    modified functionalities

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// import { GithubSearchService } from '../github-search.service';
+// import { NgForm } from '@angular/forms';
+// import { User } from '../user';
+// import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css']
 })
-export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+// export class SearchResultsComponent implements OnInit {
 
-  ngOnInit(): void {
-  }
+//   @Output() toggleBack = new EventEmitter();
 
-}
+//   user: User []= {
+
+//   }
+
+//   user:User;
+//   'hideInput':boolean;
+//   githubSearchService:GithubSearchService;
+
+//   constructor(githubSearchService:GithubSearchService) { 
+//     this.githubSearchService = githubSearchService
+
+//   }
+
+//   goBack(){
+//     this.hideInput = true;
+//     this.toggleBack.emit(this.hideInput);
+//   }
+
+//   ngOnInit(){
+//     this.user = this.githubSearchService.user; 
+//   }
+
+// }
