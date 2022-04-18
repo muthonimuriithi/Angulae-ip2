f86b622007ddb431d341a1f993ff6caf3526c1c5
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-18 11:41:00 +0300

    declarations of form and search results components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
