74b7680f6d3c19e5a8196582d8629cdf16cf8f92
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-18 18:51:30 +0300

    called out the funnction submit user name

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { GithubSearchService } from '../github-search.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-form',
@@ -8,16 +10,29 @@ import { NgForm } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
-  public showInput = true;
+  'user': User;
+  githubSearchService:GithubSearchService;
+  'username': string;
+  public showInput = true; // Assign showInput boolean value true
+  public showData = false; 
+
+  
 
   submitUsername() {
-		// this.searchGithubService.getUserData(this.username); // Passing the username entered as an arguement to getUserData function in our service then fed to the API for a response if the user exists
+		// this.githubSearchService.newUserData(this.username); // Passing the username entered as an arguement to getUserData function in our service then fed to the API for a response if the user exists
     this.showInput = false;
-    // this.showData = true;
+    this.showData = true;
 	}
+
+  showUserInput(hideInput:any) {
+    this.showInput = hideInput;
+    this.showData = false;
+  }
    
 
-  constructor() { }
+  constructor(githubSearchService:GithubSearchService) { 
+		this.githubSearchService = githubSearchService;
+	}
 
   ngOnInit(): void {
   }
