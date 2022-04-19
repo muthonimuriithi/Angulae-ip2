63b4e5debbf841c0140130f53d801fd552d2c349
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-19 20:56:16 +0300

    created path for form and search results components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:FormComponent},
+  {path: 'profile', component:FormComponent},
+  {path: 'repo', component:SearchResultsComponent}
+ 
+];
 
 @NgModule({
+
+  
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
