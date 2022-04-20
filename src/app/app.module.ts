427bef0c4d2626ae4fc8034a0aad7f3c6af8d177
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-20 16:26:51 +0300

    imported formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { GithubSearchService } from './github-search.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
-// import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FormComponent,
+    SearchResultsComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule ,
+    AppRoutingModule,
+    HttpClientModule,
     
   ],
-  providers: [],
+  providers: [GithubSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
