f1483e83ed5d1177b46a7c9463e14fe116b210cb
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-20 22:58:45 +0300

    ft:modified the apps functionality

--- src/app/github-search.service.ts
+++ src/app/github-search.service.ts
@@ -1,10 +1,69 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from  '../app/user'
+import { lastValueFrom } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubSearchService {
+  
+  user: User
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.user = new User('',0,'');
+    this.http = http;
+
+    
+    
+  }
+  
+
+
+    
+
+
+
+// update
+async getUser(username: string){
+  interface ApiResponse{
+        login:string;
+        id:number;
+        avatar_url:string;
+      }
+  
+
+   const fetchData = this.http.get<ApiResponse>('https://api.github.com/users/' + username)  
+   return await lastValueFrom(fetchData).then(response=>{
+    this.user.login = response.login
+    this.user.id = response.id
+    this.user.avatar_url = response.avatar_url
+    console.log(response)
+   return this.user
+
+   
+  })
+  .catch(err=>{
+    return err
+    // this.user.name = "Cannot find name"
+    // this.user.login = "Cannot find login information" 
+    // this.user.repos = 0
+
+
+  
+
+  })
+
+  
+  
+     
+}
+  
+
+
+  
+
+  
 }
