6a38f82f9fd8d82f471f4dc525d0dbc2982d5d3f
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-20 22:56:41 +0300

    ft:fetched data from github api

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,39 +1,43 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-// import { GithubSearchService } from '../github-search.service';
-// import { NgForm } from '@angular/forms';
-// import { User } from '../user';
-// import { HttpClient } from '@angular/common/http';
+import { GithubSearchService } from '../github-search.service';
 
 
-// @Component({
-//   selector: 'app-search-results',
-//   templateUrl: './search-results.component.html',
-//   styleUrls: ['./search-results.component.css']
-// })
-// export class SearchResultsComponent implements OnInit { 
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.css']
+})
+export class SearchResultsComponent implements OnInit {
 
-//   @Output() toggleBack = new EventEmitter();
+  @Output() toggleBack = new EventEmitter();
 
-//   user: User []= {
-    
-//   }
+  repos:any;
+  searchname:any;
 
-//   user:User;
-//   'hideInput':boolean;
-//   githubSearchService:GithubSearchService;
+  
+  
+  constructor(private githubSearchService:GithubSearchService) { 
+    this.githubSearchService = githubSearchService
 
-//   constructor(githubSearchService:GithubSearchService) { 
-//     this.githubSearchService = githubSearchService
+  }
 
-//   }
+  searchRepos(){
+    // this.githubSearchService.updateRepoSearchName(this.searchname);
+  //   this.githubSearchService.searchRepos().then(replies=>{
+  //    this.repos = replies['items'];
+  //    console.log(this.repos)
+  //  });
+  }
 
-//   goBack(){
-//     this.hideInput = true;
-//     this.toggleBack.emit(this.hideInput);
-//   }
+   
+  
 
-//   ngOnInit(){
-//     this.user = this.githubSearchService.user; 
-//   }
+ngOnInit() {
+    // this.githubSearchService.searchRepos().then(replies:any[]=>{
+    //   this.repos = replies['items'];
+    //   console.log(this.repos)
+    // });
+  }
 
-// }
+}
+
