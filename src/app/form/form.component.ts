49963eaffbf322662b349cd009cdd21087f859e0
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-21 01:33:14 +0300

    corrected indentation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,45 +1,40 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
 import { GithubSearchService } from '../github-search.service';
 import { User } from '../user';
-import { ReposService } from '../repos.service'
+import { ReposService } from '../repos.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-
-  
-  
-  username: string="muthonimuriithi";
+  username: string = '';
   public showInput = true;
-  public showData = false; 
+  public showData = false;
   profile!: User;
   repos: any;
-  
-  constructor(private githubSearchService:GithubSearchService, private reposService: ReposService) { 
-		
-	}
 
-  
+  constructor(
+    private githubSearchService: GithubSearchService,
+    private reposService: ReposService
+  ) {}
 
-  submitUsername(username:string) {
-		this.githubSearchService.getUser(username).then(result=>this.profile=result)
-    this.reposService.getRepo(username).then(output=>this.repos=output)
-    // this.showInput = false;
-    // this.showData = true;
-	}
+  submitUsername(username: string) {
+    this.githubSearchService
+      .getUser(username)
+      .then((result) => (this.profile = result));
+    this.reposService.getRepo(username).then((output) => (this.repos = output));
+   
+  }
 
-  showUserInput(hideInput:any) {
+  showUserInput(hideInput: any) {
     this.showInput = hideInput;
     this.showData = false;
   }
-   
 
-  
   ngOnInit(): void {
+    this.submitUsername("muthonimuriithi")
   }
-
 }
